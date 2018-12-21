9982634c51b506d42fcb11b1c85e7ccc145679bd
Helge Heß <devteam@zeezide.de>
2018-12-21 18:01:30 +0100

    Improve tests
    We can't lookup `ls` because the PATH is set by the
    test runner. Only stuff in /usr/bin is available.

--- Tests/ShellTests/ShellTests.swift
+++ Tests/ShellTests/ShellTests.swift
@@ -6,19 +6,30 @@ final class ShellTests: XCTestCase {
     func testSwiftVersionCall() {
         let result = shell.swift("--version")
         XCTAssert(result.stdout.hasPrefix("Apple Swift"))
+        XCTAssert(result.status == 0)
     }
   
     func testLSCall() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+        // swift runs stuff with PATH having just /usr/bin
+        let result = shell.bin.ls("/Users/")
+        XCTAssert(result.status == 0, "unexpected result: \(result)")
+        XCTAssert(!result.stdout.isEmpty, "unexpected result: \(result)")
         for file in shell.ls("/Users/").split(separator: "\n") {
             print("dir:", file)
         }
     }
+    
+    func testHostCall() {
+        let result = shell.host("zeezide.de")
+        XCTAssert(result.status == 0, "unexpected result: \(result)")
+        XCTAssert(!result.stdout.isEmpty, "unexpected result: \(result)")
+        XCTAssert(result.stdout.contains("zeezide.de"))
+        print(result.stdout)
+    }
 
     static var allTests = [
         ("testSwiftVersionCall", testSwiftVersionCall),
-        ("testLSCall",           testLSCall)
+        ("testLSCall",           testLSCall),
+        ("testHostCall",         testHostCall)
     ]
 }
